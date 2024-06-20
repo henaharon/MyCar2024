4873fa9f1620f395d9a306310dbe76040d17aba2
MoranSin <nurixhbh@gmail.com>
2024-06-21 00:07:45 +0300

    success screen done

--- App.tsx
+++ App.tsx
@@ -3,12 +3,14 @@ import React from 'react';
 import {BaseView} from './src/uiKit/BaseView';
 import AccidentReportIntro from './src/screens/AccidentReport/AccidentReportIntro';
 import CallDialogScreen from './src/screens/AccidentReport/screens/CallDialog/CallDialogScreen';
+import SuccessScreen from './src/screens/AccidentReport/screens/Success/SuccessScreen';
 
 function App(): React.JSX.Element {
   return (
     <BaseView>
       {/* <AccidentReportIntro /> */}
-      <CallDialogScreen />
+      <SuccessScreen />
+      {/* <CallDialogScreen /> */}
     </BaseView>
   );
 }
---
+++ src/screens/AccidentReport/screens/Success/SuccessScreen.js
@@ -0,0 +1,52 @@
+import { Text, View, StyleSheet } from 'react-native';
+import {BaseView, BodyBaseView} from '../../../../uiKit/BaseView';
+import {Button} from '../../components/Button'
+
+const SuccessScreen = () => {
+    return (
+        <BaseView>
+        <BodyBaseView style={{alignItems: 'center'}}>
+        <View style={styles.textContainer}>
+            <Text style={styles.headerText}>מסמך נשלחה</Text>
+            <View style={styles.centerText}>
+                <Text style={styles.confirmationText}>דוח תאונת הדרכים נשלח בהצלחה</Text>
+                <Text style={styles.confirmationText}>OirS@gmail.com למייל</Text>
+            </View>
+        </View>
+        <View style={styles.buttonContainer}>
+            <Button title={"אישור"}/>
+        </View>
+        </BodyBaseView>
+        </BaseView>
+    );
+};
+
+export default SuccessScreen;
+
+const styles = StyleSheet.create({
+    headerText: {
+        fontSize: 20,
+        fontWeight: '900',
+        color: 'black',
+    },
+    confirmationText: {
+        fontSize: 16,
+        color: 'black',
+    },
+    textContainer: {
+        alignItems: 'center',
+        position: 'relative',
+        top: '30%',
+        gap: 20,
+    },
+    buttonContainer: {
+        width: '100%',
+        position: 'relative',
+        alignItems: 'center',
+        top: '35%',
+    },
+    centerText: {
+        width: '90%',
+        alignItems: 'center',
+    }
+});
