e1702f8d6056f538940e98e913917dd654374eef
Adi Levi <adile0194@gmail.com>
2024-07-03 19:37:23 +0300

    added signature component

--- src/screens/AccidentReport/components/CarDamageInputs.js
+++ src/screens/AccidentReport/components/CarDamageInputs.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Dimensions, Text, View, StyleSheet} from 'react-native';
+import {Dimensions, View, StyleSheet} from 'react-native';
 import Title from './Title';
 import ImageInput from './ImageInput';
 import {TextareaInput} from './TextareaInput';
