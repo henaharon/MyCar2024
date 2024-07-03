c0f016d9d60b210cd9b62f22c6b4af351f655aaf
MoranSin <nurixhbh@gmail.com>
2024-07-04 00:35:28 +0300

    I7 done

--- App.tsx
+++ App.tsx
@@ -6,14 +6,14 @@ import CallDialogScreen from './src/screens/AccidentReport/screens/CallDialog/Ca
 import SuccessScreen from './src/screens/AccidentReport/screens/Success/SuccessScreen';
 import GeneralForm from './src/screens/AccidentReport/components/GeneralForm';
 import I10 from './src/screens/AccidentReport/screens/DamageReport/I10';
-import I6 from './src/screens/AccidentReport/screens/AccidentReportForm/I6';
+import I7 from './src/screens/AccidentReport/screens/AccidentReportForm/I7';
 
 function App(): React.JSX.Element {
   return (
     <BaseView>
       {/* <AccidentReportIntro /> */}
       {/* <AccidentReportIntro /> */}
-      <I6 />
+      <I7 />
     </BaseView>
   );
 }
