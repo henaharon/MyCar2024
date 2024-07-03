bb04bd5e4f9d9f01a97694d4acb28215e4650005
MoranSin <nurixhbh@gmail.com>
2024-07-03 13:03:31 +0300

    general form is component

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import {BaseView} from './src/uiKit/BaseView';
 import AccidentReportIntro from './src/screens/AccidentReport/screens/AccidentReport/AccidentReportIntro';
 import CallDialogScreen from './src/screens/AccidentReport/screens/CallDialog/CallDialogScreen';
 import SuccessScreen from './src/screens/AccidentReport/screens/Success/SuccessScreen';
-import GeneralForm from './src/screens/AccidentReport/screens/GeneralForm/GeneralForm';
+import GeneralForm from './src/screens/AccidentReport/components/GeneralForm';
 
 function App(): React.JSX.Element {
   return (
