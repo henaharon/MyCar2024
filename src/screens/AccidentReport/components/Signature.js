a236ab2f4a992160705a2f11e7339fa287951863
MoranSin <nurixhbh@gmail.com>
2024-07-05 10:47:58 +0300

    Added Adi's Components

---
+++ src/screens/AccidentReport/components/Signature.js
@@ -0,0 +1,90 @@
+import React, { useRef } from 'react';
+import { View, StyleSheet, Dimensions, Text, TouchableOpacity } from 'react-native';
+import SignatureCanvas from 'react-native-signature-canvas';
+
+const clear = 'נקה';
+const yourSignature = 'החתימה שלך';
+
+const Signature = () => {
+    const signatureRef = useRef(null);
+
+    // const handleOK = signature => {
+    //     console.log(signature);
+    // };
+
+    const handleClear = () => {
+        signatureRef.current.clearSignature();
+    };
+
+    // const handleConfirm = () => {
+    //     signatureRef.current.readSignature();
+    // };
+
+    return (
+        <View style={styles.container}>
+            <View style={styles.signatureWrapper}>
+                <SignatureCanvas
+                    ref={signatureRef}
+                    // onOK={handleOK}
+                    webStyle={styles.webStyle}
+                    style={styles.signatureContainer}
+                />
+                <TouchableOpacity style={styles.clearButton} 
+                onPress={handleClear}
+                >
+                    <Text style={styles.clearButtonText}>{clear}</Text>
+                </TouchableOpacity>
+            </View>
+        </View>
+    );
+};
+
+const windowWidth = Dimensions.get('window').width;
+const windowHeight = Dimensions.get('window').height;
+
+const styles = StyleSheet.create({
+    container: {
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    signatureWrapper: {
+        position: 'relative',
+        width: windowWidth * 0.9,
+        height: windowHeight * 0.3,
+        borderColor: '#e6e6e9',
+        borderWidth: 1,
+        borderRadius: 10,
+        overflow: 'hidden',
+        marginBottom: 20,
+    },
+    signatureContainer: {
+        width: '100%',
+        height: '100%',
+    },
+    clearButton: {
+        position: 'absolute',
+        top: 10,
+        left: 10,
+        zIndex: 3, 
+    },
+    clearButtonText: {
+        color: 'red',
+        fontSize: 16,
+    },
+    webStyle: `
+    .m-signature-pad {
+      box-shadow: none; 
+      border: none;
+    } 
+    .m-signature-pad--footer {
+      display: none; 
+      margin: 0px;
+    }
+    .m-signature-pad--body canvas {
+      stroke-width: 4px;
+      border-radius: 10px;
+    }
+  `,
+});
+
+export default Signature;
